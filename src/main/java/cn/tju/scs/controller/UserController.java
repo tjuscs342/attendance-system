f867de1ca4776853612f2cfdb93b27721aeb9d3d
lichen.ll <lichen.ll@cainiao.com>
2016-09-04 12:48:02 +0800

    implement the login function

--- src/main/java/cn/tju/scs/controller/UserController.java
+++ src/main/java/cn/tju/scs/controller/UserController.java
@@ -1,12 +1,15 @@
 package cn.tju.scs.controller;
 
+import cn.tju.scs.domain.UserDO;
 import cn.tju.scs.exception.BLLException;
 import cn.tju.scs.manager.UserManager;
 import cn.tju.scs.util.JSONBuilder;
+import com.google.common.base.Strings;
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by lichen.ll on 2016/8/31.
@@ -33,4 +36,26 @@ public class UserController {
             return JSONBuilder.buildErrorReturn( e.getErrorMessage());
         }
     }
+
+    @RequestMapping(method= RequestMethod.POST)
+    @ResponseBody
+    public Object login (String userName , String password , HttpSession session ){
+        try{
+            if(Strings.isNullOrEmpty(userName) || Strings.isNullOrEmpty(password))
+                return JSONBuilder.buildErrorReturn("用户名或密码为空");
+            UserDO userDO = userManager.getUserInfoByName( userName );
+            if( userDO == null ){
+                return JSONBuilder.buildErrorReturn("用户不存在");
+            }
+            if( !userDO.getUserName().equals(userName) || !userDO.getPassword().equals(password)){
+                return JSONBuilder.buildErrorReturn("用户名密码错误");
+            }
+            session.setAttribute("user", userDO );
+            return JSONBuilder.buildSuccessReturn(null);
+        }catch ( BLLException e ){
+            logger.error(e);
+            return JSONBuilder.buildErrorReturn( e.getMessage());
+        }
+    }
+
 }
---
+++ src/main/java/cn/tju/scs/util/StringUtils.java
@@ -0,0 +1,7 @@
+package cn.tju.scs.util;
+
+/**
+ * Created by lichen.ll on 2016/9/4.
+ */
+public class StringUtils {
+}
