3ab4bd59255c94ba698fd7466c4d69ea36b329ac
740052110 <740052110@qq.com>
2016-10-25 22:14:34 +0800

    Sick Type

--- src/main/java/cn/tju/scs/validator/ApplyTypeValidator.java
+++ src/main/java/cn/tju/scs/validator/ApplyTypeValidator.java
@@ -10,6 +10,8 @@ public class ApplyTypeValidator {
     public static boolean checkType ( Integer type ){
         if( type.equals(ApplyTypes.APPLY_YEAR))
             return true;
+        else if( type.equals(ApplyTypes.APPLY_SICK))
+            return true;
         return false;
     }
 }
